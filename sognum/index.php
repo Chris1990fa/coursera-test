6ee140862d4fb5925b2a481546a5fc85790aeaf3
Fabrizio Pezzella <fabri.pezzella@gmail.com>
2016-05-02 00:18:35 +0200

    sognum1

---
+++ sognum/index.php
@@ -0,0 +1,103 @@
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body class="#0d47a1 blue darken">
+      <!--Import jQuery before materialize.js-->
+
+<!-- Dropdown Structure -->
+<nav class="white z-depth-3">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center grey-text">Sognum</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html"></a></li>
+      </ul>
+    </div>
+  </nav>
+        
+
+          <div class="row white z-depth-3">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+          <label for="icon_prefix2">Racconta il tuo sogno</label>
+        </div>
+      </div>
+    </form>
+  </div>
+
+
+
+
+
+
+        <div class="row">
+
+
+
+        <div class="col s6 m4 l4">
+        
+        <div class="card z-depth-5 grey-text">
+            <div class="card-content ">
+              <span class="card-title">Kant</span>
+              <p>La ragione umana viene afflitta da domande che non può respingere, perché le sono assegnate dalla natura della ragione stessa, e a cui però non può neanche dare risposta, perché esse superano ogni capacità della ragione umana.</p>
+            </div>
+              <div class="card-action">
+              <a href="#" class="grey-text">Interpreta</a>
+            </div>
+          </div>  
+
+        </div>
+
+
+
+
+        
+
+        <div class="col s6 m4 l4">
+        
+        <div class="card z-depth-5 grey-text">
+            <div class="card-content">
+              <span class="card-title">Freud</span>
+              <p>La religione é un narcotico con cui l'uomo controlla la sua angoscia, ma ottunde la sua mente.</p>
+            </div>
+            <div class="card-action">
+              <a href="#" class="grey-text">Interpreta</a>
+            </div>
+          </div>  
+
+        </div>
+
+
+
+
+        <div class="col s6 m4 l4">
+        
+        <div class="card z-depth-5 grey-text">
+            <div class="card-content">
+              <span class="card-title">Cartesio</span>
+              <p>Non c'é nulla interamente in nostro potere, se non i nostri pensieri. </p>
+            </div>
+            <div class="card-action">
+              <a href="#" class="grey-text">Interpreta</a>
+            </div>
+          </div>  
+
+        </div>
+
+
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
+        
